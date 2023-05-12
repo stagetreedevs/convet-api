0a8528295a206e64b9c28d543b194b8a62534fd8
Outro-Lucas <lucasferreirapacoti@gmail.com>
2023-05-12 12:01:05 -0300

    swagger-ui-dist

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { environment } from './environment';
 import { initializeApp } from 'firebase/app';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { setupSwagger } from './swagger';
 async function bootstrap() {
   const corsConfig = {
     origin: '*',
@@ -27,24 +27,7 @@ async function bootstrap() {
   initializeApp(firebaseConfig);
 
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('CONVET API')
-    .setDescription('Convet API - Todos os endpoints da aplicação')
-    .setVersion('1.0')
-    .addTag('app')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.2/swagger-ui-bundle.min.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.2/swagger-ui-standalone-preset.min.js',
-    ],
-    customCssUrl: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.2/swagger-ui.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.2/swagger-ui-standalone-preset.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.2/swagger-ui.css',
-    ],
-  });
+  setupSwagger(app);
 
   app.enableCors()
   await app.listen(environment.PORT);
