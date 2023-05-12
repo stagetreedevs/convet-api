d235b94449e659584f1460ae51d31b31650c7055
Outro-Lucas <lucasferreirapacoti@gmail.com>
2023-05-12 15:25:30 -0300

    Aws postgreesql

--- src/environment.ts
+++ src/environment.ts
@@ -1,17 +1,9 @@
 /* eslint-disable prettier/prettier */
 export const environment = {
-  PORT:3000,
-  PG_PORT:5432,
-  PG_HOST:'motty.db.elephantsql.com',
-  PG_USER:'wpmlclgq',
-  PG_PASSWORD:'w_8JSIe7jMru2E0KlOU0sOaxLZvDJMuZ',
-  PG_DATABASE:'wpmlclgq',
+  PORT: 3000,
+  PG_PORT: 5432,
+  PG_HOST: 'convet.ct3gnj4bkybk.us-east-1.rds.amazonaws.com',
+  PG_USER: 'postgres',
+  PG_PASSWORD: 'stapi123',
+  PG_DATABASE: 'bd_convet',
 };
-// export const environment = {
-//   PORT:3000,
-//   PG_PORT:5432,
-//   PG_HOST:'127.0.0.1',
-//   PG_USER:'postgres',
-//   PG_PASSWORD:'stagetree',
-//   PG_DATABASE:'convet',
-// };
