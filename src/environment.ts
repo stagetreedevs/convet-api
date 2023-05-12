f07349c3d32b16dc3472d1a95cc3dc0fb62087e2
Outro-Lucas <lucasferreirapacoti@gmail.com>
2023-05-12 15:49:41 -0300

    VPC config

--- src/environment.ts
+++ src/environment.ts
@@ -2,16 +2,8 @@
 export const environment = {
   PORT:3000,
   PG_PORT:5432,
-  PG_HOST:'motty.db.elephantsql.com',
-  PG_USER:'wpmlclgq',
-  PG_PASSWORD:'w_8JSIe7jMru2E0KlOU0sOaxLZvDJMuZ',
-  PG_DATABASE:'wpmlclgq',
+  PG_HOST:'convet.ct3gnj4bkybk.us-east-1.rds.amazonaws.com',
+  PG_USER:'postgres',
+  PG_PASSWORD:'stapi123',
+  PG_DATABASE:'bd_convet',
 };
-// export const environment = {
-//   PORT:3000,
-//   PG_PORT:5432,
-//   PG_HOST:'127.0.0.1',
-//   PG_USER:'postgres',
-//   PG_PASSWORD:'stagetree',
-//   PG_DATABASE:'convet',
-// };
