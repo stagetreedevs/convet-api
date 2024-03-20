2c2791ccff822f1cb474ac781801e32ee24cc02d
Outro-Lucas <lucasferreirapacoti@gmail.com>
2024-03-20 15:27:34 -0300

    fix: date model

--- src/controllers/editModel/editModel.entity.ts
+++ src/controllers/editModel/editModel.entity.ts
@@ -19,15 +19,20 @@ export class EditModel {
     @Column('jsonb', { default: [] })
     materias: object[];
 
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', nullable: false })
+    date: Date;
+
     constructor(
         name: string,
         user_id: string,
         model_id: string,
+        date: Date
     ) {
         this.id = uuidv4();
         this.user_id = user_id;
         this.model_id = model_id;
         this.name = name;
         this.materias = [];
+        this.date = date;
     }
 }
