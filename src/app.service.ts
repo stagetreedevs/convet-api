26ff619cd4c09d000c03c93855b017ea288f0d36
Outro-Lucas <lucasferreirapacoti@gmail.com>
2024-06-06 18:11:15 -0300

    fix: ignore keys

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,8 +9,7 @@ export class AppService {
   ) {}
 
   getHello(): string {
-    const response = `${process.env.PORT} ${process.env.PG_PORT} ${process.env.PG_HOST} ${process.env.PG_USER} ${process.env.PG_PASSWORD} ${process.env.PG_DATABASE}`;
-    return response;
+    return 'API Convet running';
   }
 
   async getHistory(user_id: string): Promise<any> {
